32715b19f6986b8ecf1aa02cfbb84871e07b12c2
louis <935466680@qq.com>
2020-09-25 11:30:23 +0800

    '优化注释'

--- Mytools/Tools.php
+++ Mytools/Tools.php
@@ -1,8 +1,18 @@
 <?php
 namespace Mytools;
+/**
+ * @Description json工具类
+ * @author louis
+ * @email 9354666680@qq.com
+ */
 
 class Tools{
 	
+	 /**
+     * Description:  数组转化json
+     * @param array $arr 
+     * @return string 
+     */
 	public static function arrayToJson($arr){
 		if(is_array($arr)){
 		return json_encode($arr);
@@ -10,6 +20,11 @@ class Tools{
 		return false;
 		
 	}
+	 /**
+     * Description:  json字符串转化成数组
+     * @param string $json
+     * @return array
+     */
 	
     public static function jsonToArray($json){
 		
