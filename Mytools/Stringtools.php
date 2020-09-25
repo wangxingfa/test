77af8a808c414475d6b3f280aa5545ded6967b0d
louis <935466680@qq.com>
2020-09-25 15:49:59 +0800

    补充新函数 处理json

--- Mytools/Stringtools.php
+++ Mytools/Stringtools.php
@@ -277,5 +277,29 @@ class Stringtools{
         return $content;
     }
 	
+	 /**
+     * Description:  数组转化json
+     * @param array $arr 
+     * @return string 
+     */
+	public static function arrayToJson($arr){
+		if(is_array($arr)){
+		return json_encode($arr);
+		}
+		return false;
+		
+	}
+	 /**
+     * Description:  json字符串转化成数组
+     * @param string $json
+     * @return array
+     */
+	
+    public static function jsonToArray($json){
+		
+		return json_decode($json,true);
+	}
+	
+	
 	
 }
